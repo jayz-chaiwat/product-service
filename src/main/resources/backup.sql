3c623e15fe6c4ef1c21e2b272fa637e87f615ad7
Chaiwat Klomchitcharoen <chaiwat.k@appman.co.th>
2020-08-19 21:33:45 +0700

    add backup

--- src/main/resources/backup.sql
+++ src/main/resources/backup.sql
@@ -30,3 +30,10 @@ alter table detail owner to postgres;
 create unique index detail_id_uindex
 	on detail (id);
 
+INSERT INTO public.detail (id, det_name, det_value, product_id) VALUES (1, 'type', 'pizza', 1);
+INSERT INTO public.detail (id, det_name, det_value, product_id) VALUES (2, 'conutry', 'Italy', 1);
+INSERT INTO public.detail (id, det_name, det_value, product_id) VALUES (3, 'type', 'alcohol', 2);
+INSERT INTO public.detail (id, det_name, det_value, product_id) VALUES (4, 'conutry', 'Thai', 2);
+
+INSERT INTO public.product (id, product_desc, product_name) VALUES (1, 'hawaiian pizza', 'pizza');
+INSERT INTO public.product (id, product_desc, product_name) VALUES (2, 'sigha beer', 'beer');
